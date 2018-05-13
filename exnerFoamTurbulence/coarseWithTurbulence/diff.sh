457c35c0c86f464a478eaf73fd0f73aea9cf66d7
d-shipley <d.shipley.1341@gmail.com>
2018-05-13 17:34:53 +0100

    Revert "A lot has changed...forgot this was a repo.! 1. Added exnerFoam for running the basic exnerFoam solver, exnerFoam turbulence for testing the turbulent exnerFoam solver. standard is the standard rising bubble test case"
    This reverts commit 8938eb77aca7216dfdc7da92298eb770f0f30171.

--- exnerFoamTurbulence/coarseWithTurbulence/diff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-for t in [0-9]*
-do
-    sumFields -scale0 1 -scale1 -1 $t thetaDiff $t theta ../coarse/$t theta
-    sumFields -scale0 1 -scale1 -1 $t UfDiff $t Uf ../coarse/$t Uf
-done
--- exnerFoamTurbulence/standardWithTurbulence/diff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-for t in [0-9]*
-do
-    sumFields -scale0 1 -scale1 -1 $t thetaDiff $t theta ../standard/$t theta
-    sumFields -scale0 1 -scale1 -1 $t UfDiff $t Uf ../standard/$t Uf
-done
