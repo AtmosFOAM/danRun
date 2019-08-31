e61551dc299c6ffad4ba54396ec8d1675877dde6
Hilary Weller <h.weller@reading.ac.uk>
2019-08-31 05:35:40 +0100

    Updated test case for version of multiFluidBoussinesqFoam with conservative momentum transfer

--- partitioned/RayleighBenard/singleColumn/Ra_1e+05_multiFluidBoussinesqFoam_Y50_AR100_symmetricBCs_uniformSigma_gamma_conservationTesting/plots.sh
+++ partitioned/RayleighBenard/singleColumn/Ra_1e+05_multiFluidBoussinesqFoam_Y50_AR100_symmetricBCs_uniformSigma_gamma_conservationTesting/plots.sh
@@ -7,11 +7,12 @@ for field in sigma.buoyant b b.stable b.buoyant Uf Uf.buoyant Uf.stable \
          divu.stable divu.buoyant divu P Pi Pi.buoyant Pi.stable; do
     writeCellDataxyz $field -time $time
     
-    grep 1 $time/$field.xyz | grep 0.01 | sort -g -k 3 > $time/$field.xyzSorted
+    grep 1 $time/$field.xyz | grep "5  0.01" \
+        | sort -g -k 3 -u > $time/$field.xyzSorted
     mv $time/$field.xyzSorted $time/$field.xyz
     
 done
 
 # Graphs of all fields
-#python plot1Dprofile.py
+python plot1DProfile.py
 
